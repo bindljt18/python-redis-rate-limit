02e439927c0dd8230af17dcdb09a60886a5ef0e3
Jacob Bindl <jacob.bindl@tdstelecom.com>
2018-03-14 14:57:29 -0500

    Update setup.py with new name, update requirements to rely on tds-redis not regular redis.

--- setup.py
+++ setup.py
@@ -10,20 +10,10 @@ with open('requirements.txt') as f:
     requires = f.readlines()
 
 setup(
-    name='python-redis-rate-limit',
+    name='tds-python-redis-rate-limit',
     version='0.0.6',
     description=u'Python Rate Limiter based on Redis.',
     long_description=readme,
-    author=u'Victor Torres',
-    author_email=u'vpaivatorres@gmail.com',
-    url=u'https://github.com/evoluxbr/python-redis-rate-limit',
-    license=u'MIT',
     packages=find_packages(exclude=('tests', 'docs')),
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'License :: OSI Approved :: MIT License'
-    ],
     install_requires=requires
 )
